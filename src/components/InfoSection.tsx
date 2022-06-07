452815ea9828cd9c4b3c1493c42eff5bea56374a
IvanSakhman <sakhman.i@gmail.com>
2022-06-07 16:56:00 +0300

    update Membership page layout

--- src/components/InfoSection.tsx
+++ src/components/InfoSection.tsx
@@ -1,13 +1,28 @@
+import { XIcon } from '@heroicons/react/solid';
+import { Button } from '@medplum/react';
+
 interface InfoSectionProps {
   title: string | JSX.Element;
   children: JSX.Element;
+  onButtonClick?: (id: string) => void;
+  resourceType?: string;
+  id?: string;
 }
 
-const InfoSection = ({ title, children }: InfoSectionProps): JSX.Element => {
+const InfoSection = ({ title, children, onButtonClick, id = '' }: InfoSectionProps): JSX.Element => {
   return (
     <div className="mb-10 overflow-hidden border bg-white sm:rounded-md">
-      <div className="border-b border-gray-100 bg-gray-100 px-4 py-5 sm:px-6">
+      <div
+        className={`border-b border-gray-100 bg-gray-100 px-4 py-5 sm:px-6 ${
+          onButtonClick ? 'flex justify-between' : ''
+        }`}
+      >
         <h3 className="text-lg leading-6 text-gray-500">{title}</h3>
+        {onButtonClick && (
+          <Button onClick={() => onButtonClick(id)}>
+            <XIcon className="h-6 w-6 self-center text-gray-400 hover:text-red-700" />
+          </Button>
+        )}
       </div>
       <div>{children}</div>
     </div>
--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -23,10 +23,7 @@ export function SignInPage() {
           </div>
           <div className="relative py-16 px-4 sm:py-24 sm:px-6 lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:px-8 lg:py-32">
             <div className="lg:pr-8">
-              <SignInForm
-                googleClientId="679052511930-8dqur4mmg8egbttgos5pmr4ljtf3etbb.apps.googleusercontent.com"
-                onSuccess={() => navigate('/')}
-              >
+              <SignInForm onSuccess={() => navigate('/')}>
                 <h2>Sign in to Foo Medical</h2>
               </SignInForm>
             </div>
