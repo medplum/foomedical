117495b7053cb60b519282d6a761eb57f8abb84b
Reshma Khilnani <reshma.khilnani@gmail.com>
2022-07-10 11:33:08 -0700

    Updating register page and recaptcha

---
+++ src/helpers/recaptcha.ts
@@ -0,0 +1,25 @@
+import { createScriptTag } from '@medplum/react';
+
+const RECAPTCHA_SITE_KEY = '';
+
+/**
+ * Dynamically loads the recaptcha script.
+ * We do not want to load the script on page load unless the user needs it.
+ */
+export function initRecaptcha(): void {
+  if (typeof grecaptcha === 'undefined') {
+    createScriptTag('https://www.google.com/recaptcha/api.js?render=' + RECAPTCHA_SITE_KEY);
+  }
+}
+
+/**
+ * Starts a request to generate a recapcha token.
+ * @returns Promise to a recaptcha token for the current user.
+ */
+export function getRecaptcha(): Promise<string> {
+  return new Promise((resolve) => {
+    grecaptcha.ready(() => {
+      grecaptcha.execute(RECAPTCHA_SITE_KEY as string, { action: 'submit' }).then(resolve);
+    });
+  });
+}
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,36 +1,86 @@
-import { SignInForm } from '@medplum/react';
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Footer } from '../components/Footer';
+import { RegisterRequest } from '@medplum/core';
+import { OperationOutcome } from '@medplum/fhirtypes';
+import { Button, Document, Form, FormSection, Logo, Input, useMedplum } from '@medplum/react';
+import React, { useEffect, useState } from 'react';
+import { getRecaptcha, initRecaptcha } from '../helpers/recaptcha';
 
 export function RegisterPage(): JSX.Element {
-  const navigate = useNavigate();
+  const medplum = useMedplum();
+  const [outcome, setOutcome] = useState<OperationOutcome>();
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    initRecaptcha();
+  }, []);
+
   return (
-    <div id="signin" className="flex h-screen flex-col justify-between">
-      <main>
-        <div className="relative bg-white">
-          <div className="lg:absolute lg:inset-0">
-            <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-              <img
-                className="h-56 w-full object-cover lg:absolute lg:h-full"
-                src="https://images.unsplash.com/photo-1556761175-4b46a572b786?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80"
-                alt=""
-              />
-            </div>
-          </div>
-          <div className="relative py-16 px-4 sm:py-24 sm:px-6 lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:px-8 lg:py-32">
-            <div className="lg:pr-8">
-              <SignInForm
-                googleClientId="679052511930-8dqur4mmg8egbttgos5pmr4ljtf3etbb.apps.googleusercontent.com"
-                onSuccess={() => navigate('/')}
-              >
-                <h2>Register for Foo Medical</h2>
-              </SignInForm>
-            </div>
-          </div>
+    <Document width={450}>
+      <Form
+        style={{ maxWidth: 400 }}
+        onSubmit={(formData: Record<string, string>) => {
+          getRecaptcha().then((recaptchaToken: string) => {
+            medplum
+              .register({
+                ...formData,
+                projectId: '0545eccf-bc2a-4d67-8b04-b3894fd94874',
+                recaptchaToken,
+              } as unknown as RegisterRequest)
+              .then(() => setSuccess(true))
+              .catch(setOutcome);
+          });
+        }}
+      >
+        <div className="medplum-center">
+          <Logo size={32} />
+          <h1>Register new account</h1>
         </div>
-      </main>
-      <Footer />
-    </div>
+        {!success && (
+          <>
+            <FormSection title="First Name" htmlFor="firstName" outcome={outcome}>
+              <Input
+                name="firstName"
+                type="text"
+                testid="firstName"
+                required={true}
+                autoFocus={true}
+                outcome={outcome}
+              />
+            </FormSection>
+            <FormSection title="Last Name" htmlFor="lastName" outcome={outcome}>
+              <Input name="lastName" type="text" testid="lastName" required={true} outcome={outcome} />
+            </FormSection>
+            <FormSection title="Email" htmlFor="email" outcome={outcome}>
+              <Input name="email" type="email" testid="email" required={true} outcome={outcome} />
+            </FormSection>
+            <FormSection title="Password" htmlFor="password" outcome={outcome}>
+              <Input name="password" type="password" testid="password" required={true} outcome={outcome} />
+            </FormSection>
+            <p style={{ fontSize: '12px', color: '#888' }}>
+              By clicking submit you agree to the Medplum
+              <br />
+              <a href="https://www.medplum.com/privacy">Privacy Policy</a>
+              &nbsp;and&nbsp;
+              <a href="https://www.medplum.com/terms">Terms of Service</a>.
+            </p>
+            <p style={{ fontSize: '12px', color: '#888' }}>
+              This site is protected by reCAPTCHA and the Google
+              <br />
+              <a href="https://policies.google.com/privacy">Privacy Policy</a>
+              &nbsp;and&nbsp;
+              <a href="https://policies.google.com/terms">Terms of Service</a> apply.
+            </p>
+            <div className="medplum-signin-buttons">
+              <div />
+              <div>
+                <Button type="submit" testid="submit">
+                  Create account
+                </Button>
+              </div>
+            </div>
+          </>
+        )}
+        {success && <div data-testid="success">Email sent</div>}
+      </Form>
+    </Document>
   );
 }
