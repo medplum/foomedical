d97b9433862248400bb7b7448a323e68f1221612
IvanSakhman <sakhman.i@gmail.com>
2022-07-13 22:02:52 +0300

    Add get care page

---
+++ src/pages/get-care/GetCare/GetCare.tsx
@@ -0,0 +1,21 @@
+import { Scheduler } from '../../../components/Scheduler';
+import { Document, useMedplum } from '@medplum/react';
+import './GetCare.css';
+
+export default function GetCare(): JSX.Element {
+  const medplum = useMedplum();
+
+  const scheduleBundle = medplum.search('Schedule', 'actor=Patient/4b916f75-38da-42a1-9591-981cfae9098c').read();
+
+  return (
+    <>
+      {scheduleBundle.entry && scheduleBundle.entry[0].resource && (
+        <div className="flex w-full justify-center py-5">
+          <Document>
+            <Scheduler schedule={scheduleBundle.entry[0].resource} />
+          </Document>
+        </div>
+      )}
+    </>
+  );
+}
---
+++ src/pages/get-care/index.tsx
@@ -0,0 +1,15 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import PageLayout from '../../components/PageLayout';
+
+const GetCare = lazy(() => import('./GetCare/GetCare'));
+
+export default function Messages(): JSX.Element {
+  return (
+    <PageLayout>
+      <Routes>
+        <Route path="/" element={<GetCare />} />
+      </Routes>
+    </PageLayout>
+  );
+}
