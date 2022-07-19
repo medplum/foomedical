938f95d0ca3e9343ed77d79014b01d0836aab3cd
Cody Ebberson <cody@ebberson.com>
2022-07-19 13:04:11 -0700

    Reorganized MainResult

--- src/pages/health-record/MainResult.tsx
+++ src/pages/health-record/MainResult.tsx
@@ -1,20 +1,14 @@
 import { DiagnosticReport } from '@medplum/fhirtypes';
 import { DiagnosticReportDisplay, useMedplum } from '@medplum/react';
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import NoData from '../../components/NoData';
+import './MainResult.css';
 
 export default function MainResult(): JSX.Element {
   const medplum = useMedplum();
   const { resultId = '' } = useParams();
-  const [resource, setResource] = useState<DiagnosticReport>();
 
-  useEffect(() => {
-    medplum
-      .readResource('DiagnosticReport', resultId)
-      .then((value) => setResource(value as DiagnosticReport))
-      .catch((err) => console.error(err));
-  }, [medplum, resultId]);
+  const resource: DiagnosticReport = medplum.readResource('DiagnosticReport', resultId).read();
 
   return <>{resource ? <DiagnosticReportDisplay value={resource} /> : <NoData title="results" />}</>;
 }
