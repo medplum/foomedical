02e47ce20cd1a3c11fc5dbd1a4987628f03e6b0d
Cody Ebberson <cody@ebberson.com>
2022-07-27 17:34:48 -0700

    Redirect on unauthenticated

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react';
 import { MedplumClient } from '@medplum/core';
 import { MedplumProvider } from '@medplum/react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import './index.css';
 
-const medplum = new MedplumClient();
+const medplum = new MedplumClient({
+  onUnauthenticated: () => (window.location.href = '/'),
+});
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
