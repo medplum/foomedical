b22d3fcaf12fb80e5e719c072b575df6daa1b718
Cody Ebberson <cody@ebberson.com>
2022-08-28 23:05:22 -0400

    Dependency upgrades (#66)

--- src/pages/get-care/GetCare.tsx
+++ src/pages/get-care/GetCare.tsx
@@ -1,11 +1,9 @@
 import { Document, Scheduler, useMedplum } from '@medplum/react';
-import { getReferenceString, ProfileResource } from '@medplum/core';
 import NoData from '../../components/NoData';
 import './GetCare.css';
 
 export default function GetCare(): JSX.Element {
   const medplum = useMedplum();
-  const profile = medplum.getProfile() as ProfileResource;
 
   const scheduleBundle = medplum.search('Schedule').read();
 
@@ -14,7 +12,35 @@ export default function GetCare(): JSX.Element {
       {scheduleBundle.entry && scheduleBundle.entry.length > 0 ? (
         <div className="flex w-full justify-center py-2 sm:py-5">
           <Document>
-            {scheduleBundle.entry[0].resource && <Scheduler schedule={scheduleBundle.entry[0].resource} />}
+            {scheduleBundle.entry[0].resource && (
+              <Scheduler
+                schedule={scheduleBundle.entry[0].resource}
+                questionnaire={{
+                  resourceType: 'Questionnaire',
+                  name: 'Test',
+                  item: [
+                    {
+                      id: 'id-1',
+                      linkId: 'q1',
+                      type: 'string',
+                      text: 'Question 1',
+                    },
+                    {
+                      id: 'id-2',
+                      linkId: 'q2',
+                      type: 'string',
+                      text: 'Question 2',
+                    },
+                    {
+                      id: 'id-3',
+                      linkId: 'q3',
+                      type: 'string',
+                      text: 'Question 3',
+                    },
+                  ],
+                }}
+              />
+            )}
           </Document>
         </div>
       ) : (
