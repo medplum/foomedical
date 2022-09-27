9f59f249d1151c2f979af79008e84c91fc526d7f
Yury Staravoitau <yury@codexhealth.com>
2022-09-27 12:58:23 -0700

    Patameterized secret variables

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -22,9 +22,9 @@ export function RegisterPage(): JSX.Element {
             <div className="flex flex-col justify-between lg:pr-8">
               <RegisterForm
                 type="patient"
-                projectId="9602358d-eeb0-4de8-bccf-e2438b5c9162"
-                googleClientId="679052511930-8dqur4mmg8egbttgos5pmr4ljtf3etbb.apps.googleusercontent.com"
-                recaptchaSiteKey="6LfFd_8gAAAAAOCVrZQ_aF2CN5b7s91NEYIu5GxL"
+                projectId={process.env.MEDPLUM_PROJECT_ID ? process.env.MEDPLUM_PROJECT_ID : ''}
+                googleClientId={process.env.MEDPLUM_GOOGLE_CLIENT_ID}
+                recaptchaSiteKey={process.env.MEDPLUM_RECAPTCHA_SITE_KEY ? process.env.MEDPLUM_RECAPTCHA_SITE_KEY : ''}              
                 onSuccess={() => navigate('/')}
               >
                 <h2>Register with Foo Medical</h2>
--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -20,8 +20,8 @@ export function SignInPage(): JSX.Element {
           <div className="relative py-16 px-4 sm:px-6 lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:px-8">
             <div className="flex flex-col justify-between lg:pr-8">
               <SignInForm
-                projectId="9602358d-eeb0-4de8-bccf-e2438b5c9162"
-                googleClientId="679052511930-8dqur4mmg8egbttgos5pmr4ljtf3etbb.apps.googleusercontent.com"
+                projectId={process.env.MEDPLUM_PROJECT_ID}
+                googleClientId={process.env.MEDPLUM_GOOGLE_CLIENT_ID}
                 onSuccess={() => navigate('/')}
               >
                 <h2>Sign in to Foo Medical</h2>
