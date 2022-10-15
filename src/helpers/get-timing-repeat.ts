dea8faa0b2f7ffd59c1697e36815f22baa77ed53
Cody Ebberson <cody@ebberson.com>
2022-10-15 15:54:41 -0700

    Dependency upgrades

--- src/helpers/get-timing-repeat.ts
+++ src/helpers/get-timing-repeat.ts
@@ -5,8 +5,8 @@ const getTimingRepeat = (repeat?: TimingRepeat): string => {
     return 'No instruction provided';
   }
   const { frequency, period, periodUnit } = repeat;
-  const singularUnit = periodUnit === 'w' ? 'week' : 'day';
-  const pluralUnits = periodUnit === 'w' ? 'weeks' : 'days';
+  const singularUnit = periodUnit === 'wk' ? 'week' : 'day';
+  const pluralUnits = periodUnit === 'wk' ? 'weeks' : 'days';
 
   return `${frequency} ${frequency === 1 ? 'application' : 'applications'} per 
     ${period} ${period === 1 ? singularUnit : pluralUnits}`;
