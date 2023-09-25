8a4657a49a6c415f6ad1da0b90fca93ae7fa13f1
Josh Liben <josh@topology.health>
2023-09-25 16:11:08 -0400

    with newest standalone smarterfhir package

--- src/pages/Integrations.tsx
+++ src/pages/Integrations.tsx
@@ -1,33 +1,35 @@
-import { ClientFactory } from "@TopologyHealth/smarterfhir";
-import { LAUNCH } from "@TopologyHealth/smarterfhir/lib/Client/ClientFactory";
+import { ClientFactory, EMR, LAUNCH, SmartLaunchHandler } from "@TopologyHealth/smarterfhir";
+import { Button, Divider, Group, Stack, Text, Title } from "@mantine/core";
+import { Document } from "@medplum/react";
 import { useContext, useEffect, useRef } from "react";
 import { SmarterFhirContext } from "../App";
-import { Group, Stack, Title, Divider, Text, Button } from "@mantine/core";
-import { Document } from "@medplum/react";
 
 export const EpicTag = () => {
   return <Text style={{
     backgroundColor: "#db5a33", color: "white", borderRadius: "0.25rem",
-    paddingLeft: "1rem", paddingRight: "1rem", paddingTop: "0.25rem", paddingBottom: "0.25rem", 
-    fontWeight: "bold", maxWidth: "fit-content" 
+    paddingLeft: "1rem", paddingRight: "1rem", paddingTop: "0.25rem", paddingBottom: "0.25rem",
+    fontWeight: "bold", maxWidth: "fit-content"
   }}>
     Epic
   </Text>
 }
 
 async function startStandaloneLaunch() {
+  const emrClientID = "86e74d43-2046-492e-b460-73c31fa7289d"
+  const emrType: EMR = EMR.EPIC
   console.log("Launched");
-  try {
-    const emrClientID = "86e74d43-2046-492e-b460-73c31fa7289d"
-    const redirect = "http://localhost:3000/integrations";
-    const standaloneUrl = `https://fhir.epic.com/interconnect-fhir-oauth/oauth2/authorize?response_type=code&redirect_uri=${redirect}&client_id=${emrClientID}&state=TESTSTATE&aud=https%3A%2F%2Ffhir.epic.com%2Finterconnect-fhir-oauth%2Fapi%2Ffhir%2FR4`
-    window.location.href = standaloneUrl;
-  }
-  catch (e) {
-    if (e instanceof Error) {
-      throw e;
-    }
-  }
+  const smartLaunchHandler = new SmartLaunchHandler(emrClientID)
+  smartLaunchHandler.authorizeEMR(LAUNCH.STANDALONE, emrType)
+  // try {
+  //   const redirect = "http://localhost:3000/integrations";
+  //   const standaloneUrl = `https://fhir.epic.com/interconnect-fhir-oauth/oauth2/authorize?response_type=code&redirect_uri=${redirect}&client_id=${emrClientID}&state=TESTSTATE&aud=https%3A%2F%2Ffhir.epic.com%2Finterconnect-fhir-oauth%2Fapi%2Ffhir%2FR4`
+  //   window.location.href = standaloneUrl;
+  // }
+  // catch (e) {
+  //   if (e instanceof Error) {
+  //     throw e;
+  //   }
+  // }
 }
 
 export const Integrations = () => {
@@ -42,16 +44,18 @@ export const Integrations = () => {
 
     try {
       const clientFactory = new ClientFactory();
-      clientFactory.createEMRClient(LAUNCH.STANDALONE, "http://localhost:3000/integrations").then(client => {
-        setClient(client);
-        client.getPatientRead().then(v => console.log(`Successfully authenticated with Epic for patient ${v}`));
-      })
+      clientFactory.createEMRClient(LAUNCH.STANDALONE)
+        .then(client => {
+          setClient(client);
+          client.getPatientRead().then(v => console.log(`Successfully authenticated with Epic for patient ${v}`));
+        })
+        .catch(reason => cancelled  = true)
     }
-    catch {}
+    catch { }
 
     return () => { cancelled = true; }
   }, []);
-  
+
   return <div>
     <Document width={800}>
       <Title>Integrations</Title>
