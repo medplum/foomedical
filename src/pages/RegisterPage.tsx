1ae24804994c9a1bc0055985bb39f9d1cb518a22
Martin <martin.hoegl@dynapeer.com>
2025-03-12 18:36:06 +0100

    ohne background image

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -35,11 +35,7 @@ export function RegisterPage(): JSX.Element {
             <h2 style={{ textAlign: 'center', marginBottom: '2rem' }}>Register with Health Pass</h2>
           </RegisterForm>
         </div>
-      </Box>
-      <div className="background-image-container">
-        <BackgroundImage 
-          src="https://images.unsplash.com/photo-1556761175-4b46a572b786?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1567&amp;q=80"
-        />
+      </Box>    
       </div>
     </SimpleGrid>
   );
