722e4e94100fb2dbc88ceeea1439cbdc605a9c69
Martin <martin.hoegl@dynapeer.com>
2025-03-12 14:59:49 +0100

    verbindung zu app.healthpass.plus

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@
 // Replace these values with your own values for production
 export const MEDPLUM_SERVER_URL = 'https://app.healthpass.plus';
 export const MEDPLUM_PROJECT_ID = '0195672a-ba9a-7748-b255-b91904c9e6e3';
-export const MEDPLUM_GOOGLE_CLIENT_ID = '679052511930-8dqur4mmg8egbttgos5pmr4ljtf3etbb.apps.googleusercontent.com';
-export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LfFd_8gAAAAAOCVrZQ_aF2CN5b7s91NEYIu5GxL';
+export const MEDPLUM_GOOGLE_CLIENT_ID = '';
+export const MEDPLUM_RECAPTCHA_SITE_KEY = '';
