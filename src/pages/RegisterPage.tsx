c51ff3e51dca48e9a6e59c8d0adab426dc4f3796
Martin <martin.hoegl@dynapeer.com>
2025-03-12 18:18:25 +0100

    recaptcha deactivation

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,10 +1,11 @@
 import { BackgroundImage, Box, SimpleGrid } from '@mantine/core';
 import { RegisterForm } from '@medplum/react';
 import { useNavigate } from 'react-router';
-import { MEDPLUM_GOOGLE_CLIENT_ID, MEDPLUM_PROJECT_ID, MEDPLUM_RECAPTCHA_SITE_KEY } from '../config';
+import { MEDPLUM_GOOGLE_CLIENT_ID, MEDPLUM_PROJECT_ID } from '../config';
 
 export function RegisterPage(): JSX.Element {
   const navigate = useNavigate();
+  
   return (
     <SimpleGrid cols={{ base: 1, md: 2 }}>
       <Box pt={100} pb={200} p="2rem">
@@ -12,7 +13,6 @@ export function RegisterPage(): JSX.Element {
           type="patient"
           projectId={MEDPLUM_PROJECT_ID}
           googleClientId={MEDPLUM_GOOGLE_CLIENT_ID}
-          recaptchaSiteKey={MEDPLUM_RECAPTCHA_SITE_KEY}
           onSuccess={() => navigate('/')?.catch(console.error)}
         >
           <h2>Register with My Health Pass</h2>
