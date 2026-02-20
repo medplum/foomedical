c8f90b7fe34fe48f025fc87805d56578edb0d84e
Emil Gudfinnsson IPRS Group <emil.gudfinnsson@iprsgroup.com>
2026-02-20 17:52:03 +0000

    Tweak footer text for dev test

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,14 +11,14 @@ export function Footer(): JSX.Element {
         <Container p="xl">
           <Stack gap="xl">
             <SimpleGrid cols={4}>
-              <Anchor href="https://www.medplum.com/docs/tutorials/api-basics/create-fhir-data">Getting started</Anchor>
+              <Anchor href="https://www.medplum.com/docs/tutorials/api-basics/create-fhir-data">Getting started GitBash</Anchor>
               <Anchor href="https://www.medplum.com/docs/tutorials">Playing with Medplum</Anchor>
               <Anchor href="https://github.com/medplum/foomedical">Open Source</Anchor>
               <Anchor href="https://www.medplum.com/docs">Documentation</Anchor>
             </SimpleGrid>
             <Divider />
             <Text c="dimmed" size="sm">
-              &copy; {new Date().getFullYear()} Foo Medical, Inc. All rights reserved.
+              &copy; {new Date().getFullYear()} Foo Medical, Inc. All rights reserved. Hello World
             </Text>
           </Stack>
         </Container>
